f0fe8fc6cd97ae5837b4670aa625e50c4caefaa5
Hakim Boulahya <hakimblhya@gmail.com>
2017-05-16 03:14:22 +0200

    Question 1: juste les tours pour test

--- src/ulb/infofond/ChoChocapic.java
+++ src/ulb/infofond/ChoChocapic.java
@@ -6,6 +6,8 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.variables.IntVar;
 
 public class ChoChocapic {
 
@@ -40,9 +42,50 @@ public class ChoChocapic {
 
 	}
 
+	public static String fixedLength(String string) {
+		return fixedLength(string, 3);
+	}
+
+	public static String fixedLength(String string, int length) {
+		return String.format("%1$" + length + "s", string);
+	}
+
 	public static void main(String[] args) {
-		ChoChocapic choco = new ChoChocapic(args);
-		Model model = new Model("A first model");
-		System.out.println(model.getName());
+		// ChoChocapic choco = new ChoChocapic(args);
+		int k1 = 4;
+		int n = 5;
+		Model model = new Model("n=" + n + ", k1=" + k1);
+		IntVar[] trs = model.intVarArray("tr", k1, 0, n-1, false);
+		IntVar[] tls = model.intVarArray("tl", k1, 0, n-1, false);
+		for (int i = 0; i < k1; i++) {
+			for (int j = 0; j < k1; j++) {
+				if (i != j) {
+					model.arithm(trs[i], "!=", trs[j]).post();
+					model.arithm(tls[i], "!=", tls[j]).post();
+					model.arithm(trs[i], "!=", tls[j]).post();
+				}
+			}
+		}
+		Solution solution = model.getSolver().findSolution();
+		if (solution != null) {
+			System.out.println("Solution" + solution.toString());
+		} else {
+			System.out.print("No solution");
+		}
+		String[][] board = new String[n][n];
+		for (int i = 0; i < k1; i++) {
+			board[trs[i].getValue()][tls[i].getValue()] = "T";
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][j] != null) {
+					System.out.print(fixedLength(board[i][j]));
+				} else {
+					System.out.print(fixedLength("*"));
+				}
+			}
+			System.out.println();
+		}
+
 	}
 }
