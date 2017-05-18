f884c62351a390234f2302f9b18a125833aaae3b
Hakim Boulahya <hakimblhya@gmail.com>
2017-05-18 21:15:54 +0200

    Updating Museum class

--- src/ulb/infofond/MuseumSurveillance.java
+++ src/ulb/infofond/MuseumSurveillance.java
@@ -19,10 +19,20 @@ public class MuseumSurveillance {
     private final static Logger log = Logger.getLogger(MuseumSurveillance.class.getName());
 
     static {
-        log.setLevel(Level.INFO);
+        log.setLevel(Level.OFF);
     }
 
-    public static List<Character[]> getMapLines(String filename) {
+    private static Character[] parseLine(String line) {
+        List<Character> elements = new ArrayList<>();
+        for (int i = 0; i < line.length(); i += 2) {
+            elements.add(line.charAt(i));
+        }
+        Character[] array = elements.toArray(new Character[elements.size()]);
+        // log.info(Arrays.toString(array));
+        return array;
+    }
+
+    private static List<Character[]> getMapLines(String filename) {
         List<Character[]> mapLines = new ArrayList<>();
         BufferedReader reader = null;
         try {
@@ -38,7 +48,7 @@ public class MuseumSurveillance {
         return mapLines;
     }
 
-    public static Character[][] parseMap(String filename) {
+    private static Character[][] parseMap(String filename) {
         log.info(String.format("Parsing %s", filename));
         List<Character[]> lines = getMapLines(filename);
         int numberOfRows = lines.size();
@@ -50,34 +60,40 @@ public class MuseumSurveillance {
                 map[r][c] = lines.get(r)[c];
             }
         }
+        log.info(String.format("Parsing results: %s", Arrays.deepToString(map)));
         return map;
     }
 
-    private static Character[] parseLine(String line) {
-        List<Character> elements = new ArrayList<>();
-        for(int i = 0; i < line.length(); i+=2){
-            elements.add(line.charAt(i));
-        }
-        Character[] array = elements.toArray(new Character[elements.size()]);
-        log.info(Arrays.toString(array));
-        return array;
+
+    private Character[][] map;
+    private int numberOfRows, numberOfCols;
+
+    public MuseumSurveillance(String filename) {
+        System.out.println("Creating MuseumSurveillance");
+        this.map = parseMap(filename);
+        initSizeAttribute();
+        Model model = new Model("Museum Surveillance");
     }
 
-    private static void showMap(Character[][] map) {
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                System.out.print(map[i][j]);
+    private void initSizeAttribute() {
+        this.numberOfRows = this.map.length;
+        this.numberOfCols = this.map[0].length;
+    }
+
+    private void showMap() {
+        System.out.println(String.format("Map size: %dx%d", numberOfRows, numberOfCols));
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j < numberOfCols; j++) {
+                System.out.print(map[i][j] + " ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("Starting Museum Surveillance..");
-        Character[][] map = parseMap("input/museum.txt");
-        showMap(map);
-        int n = 4;
-        Model model = new Model("Museum Surveillance");
+        String filename = "input/museum.txt";
+        MuseumSurveillance solver = new MuseumSurveillance(filename);
+        solver.showMap();
 
 
     }
